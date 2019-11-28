67f818087ec4bbcf9556eada2dcf86a288f7a821
garganant <er.garganant@gmail.com>
2019-11-28 16:14:18 +0530

    Code added

---
+++ bot.py
@@ -0,0 +1,37 @@
+#https://www.youtube.com/watch?v=bf_UOFFaHiY
+
+from PIL import ImageGrab, ImageOps
+import pyautogui
+import time
+from numpy import *
+
+class Cordinates():
+    replayBtn = (340,390)
+    dinosaur = (171,395)
+    # 240 = x coordinate to check for tree
+    # 420 = y coordinate of half tree
+
+def restartGame():
+    pyautogui.click(Cordinates.replayBtn)
+
+def pressSpace():
+    pyautogui.keyDown('space')
+    time.sleep(0.05)
+    print("Jump")
+    pyautogui.keyUp('space')
+
+def imageGrab():
+    box = (Cordinates.dinosaur[0]+74,Cordinates.dinosaur[1],Cordinates.dinosaur[0]+100,Cordinates.dinosaur[1]+30)
+    image = ImageGrab.grab(box)
+    grayImage = ImageOps.grayscale(image)
+    a = array(grayImage.getcolors())
+    return a.sum()
+
+def main():
+    restartGame()
+    while True:
+        if(imageGrab()!=1447):
+            pressSpace()
+            time.sleep(0.1)
+
+main()
